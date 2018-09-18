a6608c63baddd6e3edce621b6ccb64a98c16c493
CerealBuster <raffyou@gmail.com>
2018-09-18 09:36:08 +0200

    initial commit adds .gitignore config and init.el

---
+++ init.el
@@ -0,0 +1,39 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+;;here we have the packages
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(defun load-config ()
+ "Load the actual config."
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+(load-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(package-selected-packages
+   (quote
+    (sesman cider yaml-mode which-key use-package spinner slime rainbow-delimiters queue plantuml-mode paredit org-bullets helm-projectile golden-ratio geiser flycheck fixme-mode exwm evil-mu4e evil-magit evil-leader evil-collection desktop-environment dashboard color-theme clojure-mode-extra-font-locking auto-package-update auto-complete auctex airline-themes)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
